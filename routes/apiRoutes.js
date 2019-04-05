7e7ea376d627968ad19d4bdd438b31d97594912b
nkanderson86 <N.ANDERSON.398@GMAIL.COM>
2019-04-05 09:58:21 -0700

    pushing update

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,13 @@
 let friendData = require("../data/friends");
 
 module.exports = function(app) {
+  // get request, returns all freinds in array
   app.get("/api/friends", function(req, res) {
     res.json(friendData);
+  });
+
+  //   post request, addeds new friends to array from survey
+  app.post("/api/friends", function(req, res) {
+    friendData.push(req.body);
   });
 };
